013aca7f5f52ac6faa3483de88bbe747a9a09013
Alexandr Kochurov <b2b4ly@gmail.com>
2023-02-14 00:14:18 +0600

    feat: init with new files sctucture

---
+++ src/demo/index.ts
@@ -0,0 +1,6 @@
+const initSettings = (idSettingsPanel: string) => {
+  const settings = document.querySelector(idSettingsPanel);
+  console.log('TTT settings', settings);
+};
+
+export default initSettings;
---
+++ src/plugin/index.ts
@@ -0,0 +1,7 @@
+const initSlider = (targetName: string) => {
+  const elements = document.querySelectorAll(targetName);
+
+  console.log('TTT elements', elements);
+};
+
+export default initSlider;
