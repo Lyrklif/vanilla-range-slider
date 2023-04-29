e3be83e7a5ac381e8c613487c51ff033bdc64f61
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-29 17:19:18 +0400

    chore: add plugin vite:export settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
+import replaceAllInserter from 'string.prototype.replaceall';
 
+replaceAllInserter.shim();
 const base = '/vanilla-range-slider/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createHtmlPlugin({ minify: true })],
+  plugins: [createHtmlPlugin({ minify: true }), dts()],
   base,
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    // https://vitejs.dev/guide/build.html#library-mode
+    lib: {
+      entry: path.resolve(__dirname, 'src/plugin/index.ts'),
+      name: 'vanilla-range-slider',
+      fileName: 'vanilla-range-slider',
+      formats: ['es', 'cjs', 'umd'],
+    },
+  },
 });
